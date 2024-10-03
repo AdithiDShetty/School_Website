cdfd4c5d21fb38f444d3ccacb3b7d1e2078c38f5
Karthik-Hegde-K <khegde26@gmail.com>
2024-10-03 19:36:10 +0530

    Base Files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # School_Website
 This is the ReadMe file of School Website Project.
+Please Don't modify anyhting in the Home Branch. Let it reamin as it is.
--- headerFooterManager.js
+++ headerFooterManager.js
@@ -1,1 +1,22 @@
-JavaSctipt File
+class SpecialHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <p style="display: flex; justify-content: space-around; background-color: #eeeeee; padding: 10px;">
+            <a href="home.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="event.html">Events</a>
+            <a href="academics.html">Academics</a>
+            <a href="contact.html">Contact</a>
+        </p>`
+    }
+}
+
+class SpecialFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = ""; 
+    }
+}
+
+customElements.define('special-header', SpecialHeader);
+customElements.define('special-footer', SpecialFooter);
+
